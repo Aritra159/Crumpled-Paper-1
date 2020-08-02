2428e6efe40189cc2bf33cafd476b67da78f97a8
Aritra159 <68335340+Aritra159@users.noreply.github.com>
2020-08-02 21:10:20 +0530

    Add files via upload

---
+++ Ball.1.js
@@ -0,0 +1,27 @@
+class Ball {
+    constructor(x, y, radius) {
+        var options = {
+            restitution: 0.01,
+            friction: 10.0
+        };
+        this.body = Bodies.rectangle(x, y, radius, options);
+        //this.width = width;
+        //this.height = height;
+        this.radius = radius;
+
+        World.add(world, this.body);
+    }
+    display() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        //rectMode(CENTER);
+        //strokeWeight(4);
+        //stroke("green");
+        fill(255);
+        rect(0, 0, this.radius);
+        pop();
+    }
+}
